0ab79a8eeb04f60e6a7387d2904cdb2f9c570fde
Michael Halili <97466547+haleelee@users.noreply.github.com>
2024-08-10 00:41:12 -0500

    Delete src directory

--- src/components/GetUniqueGameIDs.js
+++
@@ -1,7 +0,0 @@
-import DATA from "./season15stats.json"
-
-//  Function to determine unique gameIDs
-export const GetUniqueGameIDs = () => {
-    return [...new Set(DATA.map(item => item.game_id))];
-}
-
